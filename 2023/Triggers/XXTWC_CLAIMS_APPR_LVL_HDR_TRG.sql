6ed281a0070e0a742c8e39bd4d87ba0f93c478ac
AVISHEKGHOSH1996 <149581505+AVISHEKGHOSH1996@users.noreply.github.com>
2023-11-01 18:39:31 +0530

    Added XXTWC_CLAIMS_APPR_LVL_HDR_TRG.sql

---
+++ 2023/Triggers/XXTWC_CLAIMS_APPR_LVL_HDR_TRG.sql
@@ -0,0 +1,21 @@
+--------------------------------------------------------
+--  DDL for Trigger XXTWC_CLAIMS_APPR_LVL_HDR_TRG
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "CLAIMS"."XXTWC_CLAIMS_APPR_LVL_HDR_TRG" 
+  before insert or update on "XXTWC_CLAIMS_APPR_LVL_HDR"               
+  for each row  
+begin  
+IF INSERTING THEN  
+if :NEW."APPR_LVL_HDR_ID" is null then 
+    select "XXTWC_CLAIMS_APPR_LVL_HDR_SEQ".nextval into :NEW."APPR_LVL_HDR_ID" from sys.dual; 
+  end if; 
+ :NEW.CREATION_DATE := SYSDATE;
+  :NEW.CREATED_BY := NVL(V('P0_USER_ID'),9999);
+  ELSIF UPDATING THEN
+   :NEW.LAST_UPDATE_DATE := SYSDATE;
+   :NEW.LAST_UPDATED_BY := NVL(V('P0_USER_ID'),9999);
+  END IF;   
+END;
+/
+ALTER TRIGGER "CLAIMS"."XXTWC_CLAIMS_APPR_LVL_HDR_TRG" ENABLE;
