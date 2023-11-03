242e5f48290a902576eed50f56245b0836278818
AVISHEKGHOSH1996 <149581505+AVISHEKGHOSH1996@users.noreply.github.com>
2023-11-03 13:35:53 +0530

    Added XXTWC_CLAIMS_LPN_LINES_TRG

---
+++ 2023/Triggers/XXTWC_CLAIMS_LPN_LINES_TRG.sql
@@ -0,0 +1,20 @@
+--------------------------------------------------------
+--  DDL for Trigger XXTWC_CLAIMS_LPN_LINES
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "CLAIMS"."XXTWC_CLAIMS_LPN_LINES" 
+  before insert or update on "XXTWC_CLAIMS_LPN_LINES"               
+  for each row  
+begin  
+IF INSERTING THEN  
+    :NEW.CREATION_DATE := SYSDATE;
+    :NEW.CREATED_BY := NVL(V('P0_USER_ID'),9999);
+    :NEW.LAST_UPDATE_DATE := SYSDATE;
+    :NEW.LAST_UPDATED_BY := NVL(V('P0_USER_ID'),9999);
+  ELSIF UPDATING THEN
+   :NEW.LAST_UPDATE_DATE := SYSDATE;
+   :NEW.LAST_UPDATED_BY := NVL(V('P0_USER_ID'),9999);
+  END IF;   
+END;
+/
+ALTER TRIGGER "CLAIMS"."XXTWC_CLAIMS_LPN_LINES" ENABLE;
