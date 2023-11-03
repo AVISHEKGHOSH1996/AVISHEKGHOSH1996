e3fec040f32fa2e346bb3ea4a3e00149703688c0
AVISHEKGHOSH1996 <149581505+AVISHEKGHOSH1996@users.noreply.github.com>
2023-11-03 13:21:20 +0530

    Added XXTWC_CLAIMS_USER_ROLES_TRG.sql

---
+++ 2023/Triggers/XXTWC_CLAIMS_USER_ROLES_TRG.sql
@@ -0,0 +1,22 @@
+
+--------------------------------------------------------
+--  DDL for Trigger XXTWC_CLAIMS_USER_ROLES_TRG
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE TRIGGER "CLAIMS"."XXTWC_CLAIMS_USER_ROLES_TRG" 
+  before insert or update on "XXTWC_CLAIMS_USER_ROLES"               
+  for each row  
+begin  
+IF INSERTING THEN  
+if :NEW."USER_ROLE_ID" is null then 
+    select "XXTWC_CLAIMS_USER_ROLES_SEQ".nextval into :NEW."USER_ROLE_ID" from sys.dual; 
+  end if; 
+ :NEW.CREATION_DATE := SYSDATE;
+  :NEW.CREATED_BY := NVL(V('P0_USER_ID'),9999);
+  ELSIF UPDATING THEN
+   :NEW.LAST_UPDATE_DATE := SYSDATE;
+   :NEW.LAST_UPDATED_BY := NVL(V('P0_USER_ID'),9999);
+  END IF;   
+END;
+/
+ALTER TRIGGER "CLAIMS"."XXTWC_CLAIMS_USER_ROLES_TRG" ENABLE;
